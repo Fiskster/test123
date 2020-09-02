524110d2331d5e4713444bc411db8304e65bd12b
Axel <axel.lilja@elev.ga.ntig.se>
2020-09-02 10:53:30 +0200

    micke säger tja

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace test123
 {
@@ -6,7 +7,21 @@ namespace test123
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tja hangmantime");
+            
+            File.WriteAllText("hangman.txt","tja huremer");
+            File.WriteAllText("svar.txt","tja, det är bra");
+            string s = File.ReadAllText("hangman.txt");
+            System.Console.WriteLine("micke sa " + s);
+        
+            string b = File.ReadAllText("svar.txt");
+            System.Console.WriteLine("du svarade " + b);
+
+            System.Console.WriteLine(":)");
+
+
+
+            Console.ReadLine();
         }
     }
 }
